ef721ba96f0632085ffe1c61b84884d086ab4ebf
rmccabe62 <rmccabe1023@gmail.com>
2021-11-10 11:12:02 -0500

    add project files
    add project files

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using YourHealthNow.Areas.Identity.Data;
+using YourHealthNow.Data;
+
+[assembly: HostingStartup(typeof(YourHealthNow.Areas.Identity.IdentityHostingStartup))]
+namespace YourHealthNow.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            _ = builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<YourHealthNowContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("YourHealthNowContextConnection")));
+
+                services.AddDefaultIdentity<YourHealthNowUser>(options =>
+                {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                })
+                    .AddEntityFrameworkStores<YourHealthNowContext>();
+            });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YourHealthNow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
